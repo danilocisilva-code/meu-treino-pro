44c077224d208f450160dff9de54709e51a3f8a1
danilocisilva-code <danilocisilva@gmail.com>
2026-05-08 19:27:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
 
+st.set_page_config(page_title="Meu Treino Pro", layout="wide")
+
 def conectar():
-    # Puxa os dados com segurança dos Secrets
-    info = dict(st.secrets["gcp_service_account"])
+    # Puxa os dados dos Secrets (Formato TOML)
+    info = st.secrets["gcp_service_account"]
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
     creds = Credentials.from_service_account_info(info, scopes=scope)
     return gspread.authorize(creds)
@@ -17,6 +19,9 @@ try:
     sh = gc.open_by_key("1Q5dc9QcIRjhPn_SOHjX9LZTJtFJS2D7mjs2KjaMZXlE")
     aba = sh.get_worksheet(0)
     df = pd.DataFrame(aba.get_all_records())
-    st.dataframe(df)
+    
+    if not df.empty:
+        st.success("Conectado!")
+        st.dataframe(df)
 except Exception as e:
     st.error(f"Erro: {e}")
