f49e42517cf08a3aaf0a4afb580d8463fc1fc3bc
danilocisilva-code <danilocisilva@gmail.com>
2026-05-08 06:09:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ st.set_page_config(page_title="Meu Treino Pro", layout="wide", page_icon="🏋�
 
 # SUBSTITUA PELO SEU ID DA PLANILHA (O código entre /d/ e /edit/)
 # --- CONEXÃO COM GOOGLE SHEETS ---
-ID_PLANILHA = "1Gu49CBNiHFDBIMgt3Y-o_Nl5pGN2MRKImGAycSyJDuH0"
+# --- CONEXÃO COM GOOGLE SHEETS ---
+ID_PLANILHA = "1Q5dc9QcIRjhPn_SOHjX9LZTJtFJS2D7mjs2KjaMZXLE"
 LINK_CSV = f"https://docs.google.com/spreadsheets/d/{1Q5dc9QcIRjhPn_SOHjX9LZTJtFJS2D7mjs2KjaMZXlE}/export?format=csv&gid=0"
 
 def conectar_google():
